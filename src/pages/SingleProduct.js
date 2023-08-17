9e9803e98c24a2de4a8606db31fa072a1d14b0bb
rdtungul <rosephdarl@gmail.com>
2023-08-17 14:01:00 +0800

    adding URL Params for each single product id exisiting

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,9 +1,16 @@
-const SingleProduct = () => {
-  return (
-    <section className='section product'>
-      <h2>single product</h2>
-    </section>
-  );
-};
+import { Link, useParams } from 'react-router-dom'
 
-export default SingleProduct;
+const SingleProduct = () => {
+  // const param = useParams()
+  // console.log([param]) // it will show the inputted id in the URL
+
+  const { productID } = useParams()
+  return (
+    <section className="section product">
+      <h4>{productID}</h4>
+      <Link to="/products">back to products</Link>
+    </section>
+  )
+}
+
+export default SingleProduct
