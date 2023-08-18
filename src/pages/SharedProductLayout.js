8ebd6307000c1ac4e761ae8eda9f5b663de230f4
rdtungul <rosephdarl@gmail.com>
2023-08-18 10:39:54 +0800

    adding section class for spl file

--- src/pages/SharedProductLayout.js
+++ src/pages/SharedProductLayout.js
@@ -2,10 +2,10 @@ import { Outlet } from 'react-router-dom'
 
 const SharedProductLayout = () => {
   return (
-    <>
+    <div className="section">
       <h2>products</h2>
       <Outlet />
-    </>
+    </div>
   )
 }
 
